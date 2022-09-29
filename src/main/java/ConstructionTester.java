80032f3accf599af88de306954e4160d89a6ae7e
irukavina4309 <irukavina4309@bghscloud.org>
2022-09-29 12:28:05 -0700

    finished both problems
    did the two coding challenges

--- src/main/java/ConstructionTester.java
+++ src/main/java/ConstructionTester.java
@@ -5,6 +5,30 @@ public class ConstructionTester
 {
     public static void main(String[] args)
     {
-        // Start here!
+        Scanner scanner = new Scanner(System.in);
+
+        double taxRate;
+        double totalPrice;
+        double grandTotal;
+        int numBoards;
+        int numWindows;
+
+        System.out.println("Enter the sales tax rate:");
+        taxRate = scanner.nextDouble();
+
+        System.out.println("How many boards do you need?");
+        numBoards = scanner.nextInt();
+
+        System.out.println("How many windows do you need?");
+        numWindows = scanner.nextInt();
+
+        Construction bghsPRJ = new Construction(8,11,taxRate);
+
+        totalPrice = bghsPRJ.lumberCost(numBoards) + bghsPRJ.windowCost(numWindows);
+        grandTotal = bghsPRJ.grandTotal(totalPrice);
+
+        System.out.println("Total: " + totalPrice);
+        System.out.println("Grand Total: " + grandTotal);
+
     }
 }
--- src/main/java/HowFarAway.java
+++ src/main/java/HowFarAway.java
@@ -5,6 +5,32 @@ public class HowFarAway
 {
     public static void main(String[] args)
     {
-        // your code here.
+        Scanner scanner = new Scanner(System.in);
+
+        double startingLatitude;
+        double startingLongitude;
+        double endingLatitude;
+        double endingLongitude;
+        double distance;
+
+        System.out.print("Enter the latitude of the starting location: ");
+        startingLatitude = scanner.nextDouble();
+
+        System.out.print("Enter the longitude of the starting location: ");
+        startingLongitude = scanner.nextDouble();
+
+        System.out.print("Enter the latitude of the starting location: ");
+        endingLatitude = scanner.nextDouble();
+
+        System.out.print("Enter the longitude of the ending location: ");
+        endingLongitude = scanner.nextDouble();
+
+        GeoLocation start = new GeoLocation(startingLatitude,startingLongitude);
+        GeoLocation end = new GeoLocation(endingLatitude,endingLongitude);
+
+        distance = start.distanceFrom(end);
+
+        System.out.println("The distance is " + distance + " miles.");
+
     }
 }
